9cabdc4b6b03e9d799c0a457d8b1bed7b93c6120
kazuzukan <banghaji363@gmail.com>
2018-11-13 10:05:22 +0700

    Im add Numerical Method Archives

---
+++ Kuis1/Kuis.js
@@ -0,0 +1,7 @@
+
+<script>
+    var nilai1 = document.getElementById("T1").value;
+    var nilai2 = document.getElementById("T2").value;
+    var nilai3 = document.getElementById("T3").value;
+    var nilai4 = (parseFloat(nilai1) + parseFloat(nilai2) + parseFloat(nilai3))/3;
+</script>
